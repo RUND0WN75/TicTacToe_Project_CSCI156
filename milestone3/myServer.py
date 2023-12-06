3fd5636fb99f856f4548ecf1ac776cb2bfd8878e
Mateo Nuñez <101213022+mnunezr1@users.noreply.github.com>
2023-12-06 03:42:06 -0800

    Add files via upload

---
+++ milestone3/myServer.py
@@ -0,0 +1,525 @@
+import socket
+import threading
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ""
+port = 8080
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+playerOne = 1
+playerTwo = 2
+
+playerConn = list()
+playerAddr = list()
+
+def send_board(conn):
+    board_str = str(board)
+    conn.send("Matrix".encode())
+    conn.send(board_str.encode())
+
+def send_msg(conn, message):
+    conn.send(message.encode())
+
+def get_input(conn, currentPlayer):
+    try:
+        conn.send("Input".encode())
+        data = conn.recv(2048*10)
+        conn.settimeout(20)
+        data_decoded = data.decode().split(",")
+        x = int(data_decoded[0])
+        y = int(data_decoded[1])
+
+        board[x][y] = currentPlayer
+        send_msg(conn, "Matrix")
+        time.sleep(0.5)
+        send_msg(conn, str(board))
+    except:
+        send_msg(conn, "Error")
+        print("Error")
+
+def check_win(board):
+    winner = 0
+    # check rows
+    for i in range(3):
+       if board[i][0] == board[i][1] == board[i][2]:
+            winner = board[i][0]
+            if winner != 0:
+                break
+    
+    # check columns
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i]:
+            winner = board[0][i]
+            if winner != 0:
+                break
+
+    # check diagonals
+    if board[0][0] == board[1][1] == board[2][2]:
+        winner = board[0][0]
+    elif board[0][2] == board[1][1] == board[2][0]:
+        winner = board[0][2]
+        
+    return winner
+
+def handle_client(conn, addr, player):
+    try:
+        msg = "<<< You are player {} >>>".format(player)
+        conn.send(msg.encode())
+
+        while True:
+            send_board(conn)
+            get_input(conn, player)
+            result = check_win(board)
+
+            if result != 0 or all(cell != 0 for row in board for cell in row):
+                break
+
+        if result == 1:
+            win_msg = "Game won by Player 1"
+        elif result == 2:
+            win_msg = "Game won by Player 2"
+        elif all(cell != 0 for row in board for cell in row):
+            win_msg = "Game ended in a draw. Better luck next time"
+        else:
+            win_msg = "Error"
+
+        send_msg(conn, win_msg)
+        time.sleep(2)
+        send_msg(conn, "Over")
+
+        conn.close()
+        playerConn.remove(conn)
+        playerAddr.remove(addr)
+        print("Player {} - [{}:{}] disconnected".format(player, addr[0], str(addr[1])))
+    except socket.error as e:
+        print("Error", e)
+
+def accept_players():
+    try:
+        player = 1
+        while True:
+            conn, addr = server.accept()
+            msg1 = "<<< You are player {} >>>".format(player)
+            conn.send(msg1.encode())
+
+            playerConn.append(conn)
+            playerAddr.append(addr)
+            print("Player {} - [{}:{}] connected".format(player, addr[0], str(addr[1])))
+            
+            # Check if there are enough players to start a game
+            if len(playerConn) >= 2:
+                thread_creation(start_game)
+
+            player += 1
+    except socket.error as e:
+        print("Error", e)
+
+def start_game():
+    result = 0
+    i = 0
+    current_player = playerOne
+
+    while result == 0 and i < 9:
+        if (i % 2 == 0):
+            get_input(playerConn[0], playerOne)
+        else:
+            get_input(playerConn[1], playerTwo)
+        result = check_win(board)
+        i += 1
+        if current_player == playerOne:
+            current_player = playerTwo 
+        else:
+            current_player = playerOne
+
+    if result == 1:
+        win_msg = "Game won by Player 1"
+    elif result == 2:
+        win_msg = "Game won by Player 2"
+    elif all(cell != 0 for row in board for cell in row):
+        win_msg = "Game ended in a draw. Better luck next time"
+    else:
+        win_msg = "Error"
+
+    send_msg(playerConn[0], win_msg)
+    time.sleep(2)
+    send_msg(playerConn[1], "Over")
+    send_msg(playerConn[1], win_msg)
+    
+    time.sleep(10)
+
+    for conn in playerConn:
+        conn.close()
+
+def thread_creation(target):
+    t = threading.Thread(target=target)
+    t.daemon = True
+    t.start()
+
+def start_server():
+    try:
+        server.bind((host, port))
+        print("The server for CSCI 156 Project Group 4 has started")
+        server.listen(4)
+        accept_players()
+
+    except socket.error as e:
+        print("Error", e)
+
+start_server()
+
+
+
+""""
+import socket
+import threading
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ""
+port = 8080
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+playerOne = 1
+playerTwo = 2
+
+playerConn = list()
+playerAddr = list()
+
+def send_board_to_all():
+    board_str = str(board)
+    for conn in playerConn:
+        conn.send("Matrix".encode())
+        conn.send(board_str.encode())
+
+def send_msg(conn, message):
+    conn.send(message.encode())
+
+def get_input(conn, currentPlayer):
+    try:
+        conn.send("Input".encode())
+        data = conn.recv(2048*10)
+        conn.settimeout(20)
+        data_decoded = data.decode().split(",")
+        x = int(data_decoded[0])
+        y = int(data_decoded[1])
+
+        board[x][y] = currentPlayer
+        send_board_to_all()  # Send board updates to both players
+        time.sleep(0.5)
+        send_msg(conn, "Matrix")
+        time.sleep(0.5)
+        send_msg(conn, str(board))
+    except:
+        send_msg(conn, "Error")
+        print("Error")
+
+def check_win(board):
+    winner = 0
+    # check rows
+    for i in range(3):
+       if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            winner = board[i][0]
+            if winner != 0:
+                break
+    
+    # check columns
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            winner = board[0][i]
+            if winner != 0:
+                break
+
+    # check diagonals
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        winner = board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        winner = board[0][2]
+        
+    return winner
+
+def handle_client(conn, addr, player):
+    try:
+        msg = "<<< You are player {} >>>".format(player)
+        conn.send(msg.encode())
+
+        while True:
+            send_board_to_all()
+            get_input(conn, player)
+            result = check_win(board)
+
+            if result != 0 or all(cell != 0 for row in board for cell in row):
+                break
+
+        if result == 1:
+            win_msg = "Game won by Player 1"
+        elif result == 2:
+            win_msg = "Game won by Player 2"
+        elif all(cell != 0 for row in board for cell in row):
+            win_msg = "Game ended in a draw. Better luck next time"
+        else:
+            win_msg = "Error"
+
+        send_msg(conn, win_msg)
+        time.sleep(2)
+        send_msg(conn, "Over")
+
+        conn.close()
+        playerConn.remove(conn)
+        playerAddr.remove(addr)
+        print("Player {} - [{}:{}] disconnected".format(player, addr[0], str(addr[1])))
+    except socket.error as e:
+        print("Error", e)
+
+def accept_players():
+    try:
+        player = 1
+        while True:
+            conn, addr = server.accept()
+            msg1 = "<<< You are player {} >>>".format(player)
+            conn.send(msg1.encode())
+
+            playerConn.append(conn)
+            playerAddr.append(addr)
+            print("Player {} - [{}:{}] connected".format(player, addr[0], str(addr[1])))
+            
+            # Check if there are enough players to start a game
+            if len(playerConn) >= 2:
+                threading.Thread(target=start_game).start()
+
+            player += 1
+    except socket.error as e:
+        print("Error", e)
+
+def start_game():
+    result = 0
+    i = 0
+    current_player = playerOne
+
+    while result == 0 and i < 9:
+        if (i % 2 == 0):
+            get_input(playerConn[0], playerOne)
+        else:
+            get_input(playerConn[1], playerTwo)
+        result = check_win(board)
+        i += 1
+        if current_player == playerOne:
+            current_player = playerTwo 
+        else:
+            current_player = playerOne
+
+    if result == 1:
+        win_msg = "Game won by Player 1"
+    elif result == 2:
+        win_msg = "Game won by Player 2"
+    elif all(cell != 0 for row in board for cell in row):
+        win_msg = "Game ended in a draw. Better luck next time"
+    else:
+        win_msg = "Error"
+
+    send_msg(playerConn[0], win_msg)
+    time.sleep(2)
+    send_msg(playerConn[1], "Over")
+    send_msg(playerConn[1], win_msg)
+    
+    time.sleep(10)
+
+    for conn in playerConn:
+        conn.close()
+
+def start_server():
+    try:
+        server.bind((host, port))
+        print("The server for CSCI 156 Project Group 4 has started")
+        server.listen(5)
+        accept_players()
+
+    except socket.error as e:
+        print("Error", e)
+
+start_server()
+
+"""
+
+
+
+
+""""
+import socket
+import threading
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ""
+port = 8080
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+playerOne = 1
+playerTwo = 2
+
+playerConn = list()
+playerAddr = list()
+
+def list_connected_players():
+    print("Available Players:")
+    for i, addr in enumerate(playerAddr, start=1):
+        print(f"Player {i} - {addr[0]}:{addr[1]}")
+
+def thread_creation(target):
+    t = threading.Thread(target=target)
+    t.daemon = True
+    t.start()
+
+def send_board(conn):
+    board_str = str(board)
+    conn.send("Matrix".encode())
+    conn.send(board_str.encode())
+
+def send_board_to_all():
+    board_str = str(board)
+    for conn in playerConn:
+        conn.send("Matrix".encode())
+        conn.send(board_str.encode())
+
+def send_msg(conn, message):
+    conn.send(message.encode())
+
+def get_input(conn, currentPlayer):
+    try:
+        conn.send("Input".encode())
+        data = conn.recv(2048*10)
+        conn.settimeout(20)
+        data_decoded = data.decode().split(",")
+        x = int(data_decoded[0])
+        y = int(data_decoded[1])
+
+        board[x][y] = currentPlayer
+        send_board_to_all()  # Send board updates to both players
+        time.sleep(0.5)
+        send_msg(conn, "Matrix")
+        time.sleep(0.5)
+        send_msg(conn, str(board))
+    except:
+        send_msg(conn, "Error")
+        print("Error")
+
+def check_win(board):
+    winner = 0
+    # check rows
+    for i in range(3):
+       if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            winner = board[i][0]
+            if winner != 0:
+                break
+    
+    # check columns
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            winner = board[0][i]
+            if winner != 0:
+                break
+
+    # check diagonals
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        winner = board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        winner = board[0][2]
+        
+    return winner
+
+def handle_client(conn, addr, player):
+    try:
+        msg = "<<< You are player {} >>>".format(player)
+        conn.send(msg.encode())
+
+        while True:
+            send_board(conn)
+            get_input(conn, player)
+            result = check_win(board)
+
+            if result != 0 or all(cell != 0 for row in board for cell in row):
+                break
+
+        if result == 1:
+            win_msg = "Game won by Player 1"
+        elif result == 2:
+            win_msg = "Game won by Player 2"
+        elif all(cell != 0 for row in board for cell in row):
+            win_msg = "Game ended in a draw. Better luck next time"
+        else:
+            win_msg = "Error"
+
+        send_msg(conn, win_msg)
+        time.sleep(2)
+        send_msg(conn, "Over")
+
+        conn.close()
+        playerConn.remove(conn)
+        playerAddr.remove(addr)
+        print("Player {} - [{}:{}] disconnected".format(player, addr[0], str(addr[1])))
+    except socket.error as e:
+        print("Error", e)
+
+def start_server():
+    try:
+        server.bind((host, port))
+        print("The server for CSCI 156 Project Group 4 has started")
+        server.listen(5)
+        accept_players()
+
+    except socket.error as e:
+        print("Error", e)
+
+def accept_players():
+    try:
+        while True:
+            conn, addr = server.accept()
+            playerConn.append(conn)
+            playerAddr.append(addr)
+
+            list_connected_players()
+
+            if len(playerConn) >= 2:
+                thread_creation(start_game)
+
+    except socket.error as e:
+        print("Error", e)
+
+def start_game():
+    result = 0
+    i = 0
+    current_player = playerOne
+
+    while result == 0 and i < 9:
+        if (i % 2 == 0):
+            get_input(playerConn[0], playerOne)
+        else:
+            get_input(playerConn[1], playerTwo)
+        result = check_win(board)
+        i += 1
+        if current_player == playerOne:
+            current_player = playerTwo 
+        else:
+            current_player = playerOne
+
+    if result == 1:
+        win_msg = "Game won by Player 1"
+    elif result == 2:
+        win_msg = "Game won by Player 2"
+    elif all(cell != 0 for row in board for cell in row):
+        win_msg = "Game ended in a draw. Better luck next time"
+    else:
+        win_msg = "Error"
+
+    send_msg(playerConn[0], win_msg)
+    time.sleep(2)
+    send_msg(playerConn[1], "Over")
+    send_msg(playerConn[1], win_msg)
+    
+    time.sleep(10)
+
+    for conn in playerConn:
+        conn.close()
+
+start_server()
+"""
---
+++ milestone3/tictactoe.py
@@ -0,0 +1,166 @@
+import pygame
+import random
+
+pygame.init()
+
+Width = 500
+Height = 500
+Cell = Width // 3
+
+White = (255, 255, 255)
+Lines = (0,0,0)
+
+screen = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("Tic Tac Toe CSCI 156 Group 4")
+font = pygame.font.Font(pygame.font.get_default_font(), 20)
+
+def screen_board(board, message = None, delay = None):
+    screen.fill(White)
+
+    for i in range(1, 4):
+        pygame.draw.line(screen, Lines, (i * Cell, 0), (i * Cell, Height), 2)
+        pygame.draw.line(screen, Lines, (0, i* Cell), (Width, i * Cell), 2)
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 1:
+                pygame.draw.line(screen, Lines, (j * Cell, i * Cell), ((j + 1) * Cell, (i + 1) * Cell), 2)
+                pygame.draw.line(screen, Lines, ((j + 1) * Cell, i * Cell), (j * Cell, (i + 1) * Cell), 2)
+            elif board[i][j] == 2:
+                pygame.draw.circle(screen, Lines, (j * Cell + Cell // 2, i * Cell + Cell // 2), Cell // 2 - 2, 2)
+
+    if message:
+        sentence = font.render(message, True, Lines)
+        alignment = sentence.get_rect(center = (Width //2, Height // 10))
+        screen.blit(sentence, alignment)
+    
+    pygame.display.flip()
+
+    if delay is not None:
+        pygame.time.delay(delay)
+    else:
+        pygame.time.delay(0)
+
+
+def player_input(board, mouseX, mouseY):
+    row = mouseY // Cell
+    col = mouseX // Cell
+
+    # Check that the cell is not already in use
+    if board[row][col] == 0:
+        board[row][col] = 1
+        return True
+
+    return False
+
+def computer_move(board):
+    # Randomly generated
+    while True:
+        row, col = random.randint(0, 2), random.randint(0, 2)
+        if board[row][col] == 0:
+            board[row][col] = 2
+            return
+
+def player_in(board):
+    r = int(input("Enter row:\n")) 
+    c= int(input("Enter column:\n"))
+
+    # check that cell is not is use
+    while board[r-1][c-1] != 0:
+        print("Cell already taken. Please try again.")
+        r = int(input("Enter row:\n")) 
+        c= int(input("Enter column:\n"))
+
+    # place
+    board[r-1][c-1] = 1
+
+def computer_move(board):
+    #randomly generated
+    r, c = random.randint(0, 2), random.randint(0, 2)
+
+    while board[r][c] != 0:
+        r, c = random.randint(0, 2), random.randint(0, 2)
+
+    board[r][c] = 2
+    print(f"Computer played O at Row: {r + 1} Column: {c + 1}")
+
+def check_win(board):
+    # check rows
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != 0:
+            return True
+
+    # check columns
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] != 0:
+            return True
+
+    # check negative diagonal
+    if board[0][0] == board[1][1] == board[2][2] != 0:
+        return True
+
+    #check positive diagonal
+    if board[0][2] == board[1][1] == board[2][0] != 0:
+        return True
+
+    return False
+
+def reset_board(board):
+    for i in range(3):
+        for j in range(3):
+            board[i][j] = 0
+
+def display_board(board):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                print(' ', end='')
+            elif board[i][j] == 1:
+                print('X', end='')
+            elif board[i][j] == 2:
+                print('O', end='')
+
+            if j < 2:
+                print('|', end='')
+            else:
+                print()
+
+        if i < 2:
+            print("-----")
+
+def main():
+    # board uses a 2D array implementation
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    
+    running = True
+    screen_board(board)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                if player_input(board, mouseX, mouseY):
+                    if check_win(board):
+                        screen_board(board)
+                        screen_board(board, "Congratulations: You won the game!", 3000)
+                        running = False
+                    elif all(cell != 0 for row in board for cell in row):
+                        screen_board(board)
+                        screen_board(board, "Game ended in a draw. Better luck next time.", 3000)
+                        running = False
+                    else:
+                        computer_move(board)
+                        if check_win(board):
+                            screen_board(board)
+                            screen_board(board, "Computer won the game. Better luck next time.", 3000)
+                            running = False
+                        elif all(cell != 0 for row in board for cell in row):
+                            screen_board(board, "Game ended in a draw. Better luck next time.", 3000)
+                            running = False
+                    screen_board(board)
+
+    pygame.quit()  
+
+if __name__ == "__main__":
+    main()
